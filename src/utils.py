9366eaec5162b623e3ada5c169340d3c8d3a6885
Andrea Blushi <andreablushi@gmail.com>
2025-12-10 17:25:33 +0100

    Created utils.py

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import pm4py
+import sklearn as skl
+import pandas as pd
+
+
+
+def extract_recommendations(tree, feature_names, class_values, prefix_set):
+    '''
+    Extract recommendations from a decision tree for given prefixes.
+    :param tree: Decision tree classifier
+    :param feature_names: List of feature names
+    :param class_values: List of class values
+    :param prefix_set: Set of prefixes to extract recommendations for 
+    '''
+    return 0
+
+def evaluate_recommendations(test_set, recommendations):
+    return 0
+
